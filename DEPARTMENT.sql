75552eb8a2608ffdd7f16295cac3d1257bb5697c
Jadhao <kunal-anna.jadhao@capgemini.com>
2023-01-19 17:42:17 +0530

    success

---
+++ DEPARTMENT.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Department (
+  deptId INTEGER PRIMARY KEY,
+  deptname TEXT NOT NULL,
+  deptloc TEXT NOT NULL
+);
+
+-- insert
+INSERT INTO Department VALUES(222,'dept1','PUNE');
+INSERT INTO Department VALUES(333,'dept2','MUMBAI');
+INSERT INTO Department VALUES(444,'dept6','PUNE');
+INSERT INTO Department VALUES(555,'dept0','PUNE');
+INSERT INTO Department VALUES(777,'dept1','HAYDRABAD');
+
