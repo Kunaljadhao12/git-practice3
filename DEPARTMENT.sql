ccee116456eff6bf8dc5fce1429a9ec193ddba16
Kunaljadhao12 <122590336+Kunaljadhao12@users.noreply.github.com>
2023-01-19 18:00:53 +0530

    Update DEPARTMENT.sql

--- DEPARTMENT.sql
+++ DEPARTMENT.sql
@@ -9,5 +9,5 @@ INSERT INTO Department VALUES(222,'dept1','PUNE');
 INSERT INTO Department VALUES(333,'dept2','MUMBAI');
 INSERT INTO Department VALUES(444,'dept6','PUNE');
 INSERT INTO Department VALUES(555,'dept0','PUNE');
-INSERT INTO Department VALUES(777,'dept1','HAYDRABAD');
+INSERT INTO Department VALUES(777,'dept1','Hyderabad');
 
